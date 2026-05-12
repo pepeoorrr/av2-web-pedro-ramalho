07d897b17a2aadf5440f601c92c0c9f6e5fd0df2
Pedro Oliveira Rios Ramalho <pedro.oramalho@eaportal.org>
2026-05-12 09:18:33 -0300

    Sua mensagem aqui

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -3,11 +3,7 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../../generated/prisma/index.js";
 
 const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  connectionLimit: 5
+  url: process.env.DATABASE_URL
 });
 const prisma = new PrismaClient({ adapter });
 
